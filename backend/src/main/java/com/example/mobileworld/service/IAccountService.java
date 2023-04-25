ae2005e7872aacde0eb1f0e99073277c04e862d8
Hiep <hiepbon0000@gmail.com>
2023-04-25 13:36:00 +0700

    Update IAccountService.java

--- backend/src/main/java/com/example/mobileworld/service/IAccountService.java
+++ backend/src/main/java/com/example/mobileworld/service/IAccountService.java
@@ -1,13 +1,12 @@
 package com.example.mobileworld.service;
 
 
-import com.example.mobileworld.dto.AccountDTO;
 import com.example.mobileworld.dto.SignupDTO;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
 public interface IAccountService extends UserDetailsService {
 
-
     void signUp(SignupDTO acDTO) throws Exception;
+
     void active(String token) throws Exception;
 }
