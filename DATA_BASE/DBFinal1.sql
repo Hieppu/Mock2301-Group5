874ddcffa3c100543484bb9fd854e4304734bafb
Tuan Dang <tuandang2k2@gmail.com>
2023-05-22 18:21:56 +0700

    add DB
    add DB

--- DATA_BASE/DBFinal1.sql
+++ DATA_BASE/DBFinal1.sql
@@ -9,7 +9,7 @@ CREATE TABLE IF NOT EXISTS `Account` (
 	`username`	 	CHAR(50) NOT NULL UNIQUE CHECK (LENGTH(`username`) >= 6 AND LENGTH(`username`) <= 50),
     `password` 		VARCHAR(800) NOT NULL,
     `first_name` 	NVARCHAR(50) NOT NULL,
-    `last_name` 		NVARCHAR(50) NOT NULL,
+    `last_name` 	NVARCHAR(50) NOT NULL,
 	`email` 		CHAR(50) NOT NULL UNIQUE CHECK (LENGTH(`email`) >= 6 AND LENGTH(`email`) <= 50),
     `role` 			ENUM('ADMIN','EMPLOYEE','MANAGER'),
     `avatar_url`   	VARCHAR(500),
