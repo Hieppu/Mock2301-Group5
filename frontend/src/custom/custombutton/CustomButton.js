9ef312ea2de922877b8a02c1e81008df85fe6e2f
Tuan Dang <tuandang2k2@gmail.com>
2023-05-22 18:12:38 +0700

    add login backend
    add login backend

--- frontend/src/custom/custombutton/CustomButton.js
+++
@@ -1,5 +0,0 @@
-import './CustomButton.scss'
-
-const Button = ({ children, handleClick }) => <button className='button' onClick={handleClick}>{children}</button>
-
-export default Button
--- frontend/src/page/features/cart/Cart.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Cart = () => {
-  return (
-    <div>Cartsadsadsadsadsasdsdsdd</div>
-  )
-}
-
-export default Cart
